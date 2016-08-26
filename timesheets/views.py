9d4f8093a5dfa90e49630e7f5c5318e4f725f411
Kevin Truong <aa0411177846@hotmail.com>
2016-08-26 18:52:31 +1000

    render html

--- timesheets/views.py
+++ timesheets/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    """
-    Returns a basic response.
-    :param request: Django http request
-    :return:
-    """
-    return HttpResponse("If this works then you are running the server correctly! Welcome to SEPGroup8 HELLO ALL TEST 2")
+    template = loader.get_template('timesheets/index.html')
+
+#    return HttpResponse(template.render('', request))
+
