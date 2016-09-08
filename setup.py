0653304ea41ec8023008e42dc07220b4807fa82f
Kevin Truong <aa0411177846@hotmail.com>
2016-09-08 10:43:42 +1000

    Latest update before presentation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sep',
+    version='1.00',
+    packages=['sep', 'timesheets', 'timesheets.migrations'],
+    url='',
+    license='',
+    author='Kevin',
+    author_email='kevintruong94@hotmail.com',
+    description=''
+)
