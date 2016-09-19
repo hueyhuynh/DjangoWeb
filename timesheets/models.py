38ecc9a157527afd57d94b0c2b3471eed51177c9
Mackay <mackay@example.com>
2016-09-19 17:37:12 +1000

    Add skeleton Timesheet model

--- timesheets/models.py
+++ timesheets/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Timesheet(models.Model):
+    total_hours_worked = models.IntegerField()
+    total_hours_break = models.IntegerField()
+    submission_date = models.DateTimeField('Date the timesheet was submitted by the employee')
+    approval_date = models.DateTimeField('Date the timesheet was approved by the Approving Manager')
