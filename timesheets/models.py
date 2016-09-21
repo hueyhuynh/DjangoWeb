1a36ff9dbabdd2d6380c8c7ea180e95c0896e4b7
Kevin Truong <aa0411177846@hotmail.com>
2016-09-21 10:54:34 +1000

    timesheet modal

--- timesheets/models.py
+++ timesheets/models.py
@@ -1,5 +1,7 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Timesheet(models.Model):
+    date = models.DateField('date of timesheet')
+    time_hour = models.IntegerField()
+    comment = models.CharField(max_length=1000)
+    submission = models.DateTimeField('date of submission')
